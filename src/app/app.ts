d62968dd9fc7f84887b3464027b02a723b7ed610
anacirnu <anamaria.cirnu10@gmail.com>
2025-12-04 05:14:01 +0100

    input form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common'; 
+import { HttpClientModule } from '@angular/common/http';
+
+import { Contact } from './contact.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('contact-list-app');
+  contacts: Contact[] = [];
+
+  private readonly phonePattern = /^\d{10}$/;
+
+  contactForm = new FormGroup({
+  name: new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]),
+
+  phone: new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.phonePattern) 
+  ]),
+});
+
+  onSubmit() {
+    console.log('Form Submitted!');
+  }
 }
